4f2860ee77a6cbfb3c9f29aac574e156516a79c1
Ahsan-codelife <65127583+Ahsan-codelife@users.noreply.github.com>
2020-07-17 22:09:22 +0600

    done

--- assignment.js
+++ assignment.js
@@ -22,6 +22,28 @@ console.log(woodNeed);
 
 // brickCalculator
 
+function brickCalculator(building) {
+    var feetBricks = 1000;
+    if (building < 10) {
+        var totalFeet = (building * 15);
+        var totalBricks = (totalFeet + feetBricks);
+        return totalBricks;
+    }
+    else if (building <= 20) {
+        var firstTenFloorBricks = 30000;
+        var totalFeet = (building * 12);
+        var totalBricks = (totalFeet * feetBricks) + firstTenFloorBricks;
+        return totalBricks;
+    }
+    else if (building >= 21) {
+        var firstTenFloorBricks = 50000;
+        var secondFloorBricks = 20000;
+        var extraTotalBricks = (firstTenFloorBricks + secondFloorBricks);
+        totalFeet = (building * 10);
+        var totalBricks = (totalFeet * feetBricks) + extraTotalBricks;
+        return totalBricks;
+    }
+}
 
 
 
@@ -29,16 +51,16 @@ console.log(woodNeed);
 
 // tinyFriend
 
-function tinyFriend(friend){
+function tinyFriend(friend) {
     var smallName = Infinity;
     var tinyName;
-    for (var i=0; i<friend.length; i++) 
-    {
-        if (friend[i].length < smallName)
-    {
-        smallName = friend[i].length;
-        tinyFriend = friend[i];
+    for (var i = 0; i < friend.length; i++) {
+        if (friend[i].length < smallName) {
+            smallName = friend[i].length;
+            tinyName = friend[i];
+        }
     }
-}
- return tinyName;
-}
+    return tinyName;
+ }
+ var smallestName = tinyFriend(['alu', 'piyag','potol', 'jinga','s', 'ba']);
+ console.log(smallestName);
